f74e1b1faecd40af94f083b9092fc35318bbe7eb
Jathan McCollum <jathan@gmail.com>
2021-11-05 08:47:02 -0700

    Implemented unit tests for HashiCorp Vault provider (#7)
    * Implemented unit tests for HashiCorp Vault provider
    
    - Refactored provider code a little for more robust error-handling when secret cannot be found or when key is incorrect
    - Added a `nautobot_config.py` with `PLUGINS_CONFIG` for use in testing
    - Added `requests-mock`, `hvac`, and `boto3` as dev. dependencies in `pyproject.toml`
    - Make testing a little more robust for HashiCorp Vault with differing key/path names
    - Do the same for AWS
    - Added a new negative parameters test
    
    Co-authored-by: Glenn Matthews <glenn.matthews@networktocode.com>

--- nautobot_secrets_providers/providers/hashicorp.py
+++ nautobot_secrets_providers/providers/hashicorp.py
@@ -7,8 +7,7 @@ except ImportError:
     hvac = None
 
 from nautobot.utilities.forms import BootstrapMixin
-from nautobot.extras.secrets import SecretsProvider
-from nautobot.extras.secrets.exceptions import SecretProviderError
+from nautobot.extras.secrets import exceptions, SecretsProvider
 
 
 __all__ = ("HashiCorpVaultSecretsProvider",)
@@ -38,14 +37,34 @@ class HashiCorpVaultSecretsProvider(SecretsProvider):
         """
         Return the value stored under the secret’s key in the secret’s path.
         """
+        # This is only required for HashiCorp Vault therefore not defined in
+        # `required_settings` for the plugin config.
         plugin_settings = settings.PLUGINS_CONFIG["nautobot_secrets_providers"]
         if "hashicorp_vault" not in plugin_settings:
-            raise SecretProviderError(secret, cls, "HashiCorp Vault is not configured!")
+            raise exceptions.SecretProviderError(secret, cls, "HashiCorp Vault is not configured!")
 
-        plugin_settings = plugin_settings["hashicorp_vault"]
-        if "url" not in plugin_settings or "token" not in plugin_settings:
-            raise SecretProviderError(secret, cls, "HashiCorp Vault is not configured!")
+        vault_settings = plugin_settings["hashicorp_vault"]
+        if "url" not in vault_settings or "token" not in vault_settings:
+            raise exceptions.SecretProviderError(secret, cls, "HashiCorp Vault is not configured!")
 
-        client = hvac.Client(url=plugin_settings["url"], token=plugin_settings["token"])
-        vault = client.secrets.kv.read_secret(path=secret.parameters.get("path"))
-        return vault["data"]["data"][secret.parameters.get("key")]
+        # Try to get parameters and error out early.
+        try:
+            secret_path = secret.parameters["path"]
+            secret_key = secret.parameters["key"]
+        except KeyError as err:
+            msg = f"The secret parameter could not be retrieved for field {err}"
+            raise exceptions.SecretParametersError(secret, cls, msg) from err
+
+        # Get the client and attempt to retrieve the secret.
+        client = hvac.Client(url=vault_settings["url"], token=vault_settings["token"])
+        try:
+            response = client.secrets.kv.read_secret(path=secret_path)
+        except hvac.exceptions.InvalidPath as err:
+            raise exceptions.SecretValueNotFoundError(secret, cls, str(err)) from err
+
+        # Retrieve the value using the key or complain loudly.
+        try:
+            return response["data"]["data"][secret_key]
+        except KeyError as err:
+            msg = f"The secret value could not be retrieved using key {err}"
+            raise exceptions.SecretValueNotFoundError(secret, cls, msg) from err
--- nautobot_secrets_providers/tests/test_providers.py
+++ nautobot_secrets_providers/tests/test_providers.py
@@ -4,10 +4,12 @@ import boto3
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase, tag
 from moto import mock_secretsmanager
+import requests_mock
 
 from nautobot.extras.models import Secret
 from nautobot.extras.secrets import exceptions
-from nautobot_secrets_providers.providers import AWSSecretsManagerSecretsProvider
+from nautobot_secrets_providers.providers import AWSSecretsManagerSecretsProvider, HashiCorpVaultSecretsProvider
+
 
 # Use the proper swappable User model
 User = get_user_model()
@@ -44,14 +46,14 @@ class AWSSecretsManagerSecretsProviderTestCase(SecretsProviderTestCase):
             name="hello-aws",
             slug="hello-aws",
             provider=self.provider.slug,
-            parameters={"name": "hello", "region": "us-east-2", "key": "hello"},
+            parameters={"name": "hello", "region": "us-east-2", "key": "location"},
         )
 
     @mock_secretsmanager
     def test_retrieve_success(self):
         """Retrieve a secret successfully."""
         conn = boto3.client("secretsmanager", region_name=self.secret.parameters["region"])
-        conn.create_secret(Name="hello", SecretString='{"hello":"world"}')
+        conn.create_secret(Name="hello", SecretString='{"location":"world"}')
 
         result = self.provider.get_value_for_secret(self.secret)
         self.assertEquals(result, "world")
@@ -71,7 +73,7 @@ class AWSSecretsManagerSecretsProviderTestCase(SecretsProviderTestCase):
     def test_retrieve_does_not_match(self):
         """Try and fail to retrieve the wrong secret."""
         conn = boto3.client("secretsmanager", region_name=self.secret.parameters["region"])
-        conn.create_secret(Name="bogus", SecretString='{"hello":"world"}')
+        conn.create_secret(Name="bogus", SecretString='{"location":"world"}')
 
         with self.assertRaises(exceptions.SecretValueNotFoundError) as err:
             self.provider.get_value_for_secret(self.secret)
@@ -84,6 +86,97 @@ class AWSSecretsManagerSecretsProviderTestCase(SecretsProviderTestCase):
         """Try and fail to retrieve the wrong secret."""
         conn = boto3.client("secretsmanager", region_name=self.secret.parameters["region"])
         conn.create_secret(Name="hello", SecretString='{"fake":"notreal"}')
+
+        with self.assertRaises(exceptions.SecretValueNotFoundError) as err:
+            self.provider.get_value_for_secret(self.secret)
+
+        exc = err.exception
+        self.assertIn(self.secret.parameters["key"], exc.message)
+
+
+class HashiCorpVaultSecretsProviderTestCase(SecretsProviderTestCase):
+    """Tests for HashiCorpVaultSecretsProvider."""
+
+    provider = HashiCorpVaultSecretsProvider
+
+    # Mock API response
+    mock_response = {
+        "request_id": "4708ebf3-3bce-b30e-0601-b192bf47af17",
+        "lease_id": "",
+        "renewable": False,
+        "lease_duration": 0,
+        "data": {
+            "data": {
+                "location": "world",
+            },
+            "metadata": {
+                "created_time": "2021-10-28T22:43:47.829676011Z",
+                "deletion_time": "",
+                "destroyed": False,
+                "version": 2,
+            },
+        },
+        "wrap_info": None,
+        "warnings": None,
+        "auth": None,
+    }
+
+    def setUp(self):
+        super().setUp()
+
+        # The secret we be using.
+        self.secret = Secret.objects.create(
+            name="hello-hashicorp",
+            slug="hello-hashicorp",
+            provider=self.provider.slug,
+            parameters={"path": "hello", "key": "location"},
+        )
+        self.test_path = "http://localhost:8200/v1/secret/data/hello"
+
+    @requests_mock.Mocker()
+    def test_retrieve_success(self, requests_mocker):
+        """Retrieve a secret successfully."""
+        requests_mocker.register_uri(method="GET", url=self.test_path, json=self.mock_response)
+
+        response = self.provider.get_value_for_secret(self.secret)
+        self.assertEquals(self.mock_response["data"]["data"]["location"], response)
+
+    @requests_mock.Mocker()
+    def test_retrieve_invalid_parameters(self, requests_mocker):
+        """Try and fail to retrieve a secret with incorrect parameters."""
+        bogus_secret = Secret.objects.create(
+            name="bogus-hashicorp",
+            slug="bogus-hashicorp",
+            provider=AWSSecretsManagerSecretsProvider,  # Wrong provider
+            parameters={"name": "hello", "region": "us-east-2", "key": "hello"}, # Wrong params
+        )
+
+        requests_mocker.register_uri(method="GET", url=self.test_path, json=self.mock_response)
+
+        with self.assertRaises(exceptions.SecretParametersError) as err:
+            self.provider.get_value_for_secret(bogus_secret)
+
+        exc = err.exception
+        self.assertIn("path", exc.message)
+
+    @requests_mock.Mocker()
+    def test_retrieve_does_not_exist(self, requests_mocker):
+        """Try and fail to retrieve a secret that doesn't exist."""
+        self.secret.parameters["path"] = "bogus"
+        bogus_path = self.test_path.replace("hello", "bogus")
+        requests_mocker.register_uri(method="GET", url=bogus_path, status_code=404)
+
+        with self.assertRaises(exceptions.SecretValueNotFoundError) as err:
+            self.provider.get_value_for_secret(self.secret)
+
+        exc = err.exception
+        self.assertIn("bogus", exc.message)
+
+    @requests_mock.Mocker()
+    def test_retrieve_invalid_key(self, requests_mocker):
+        """Try and fail to retrieve the wrong secret."""
+        self.secret.parameters["key"] = "bogus"
+        requests_mocker.register_uri(method="GET", url=self.test_path, json=self.mock_response)
 
         with self.assertRaises(exceptions.SecretValueNotFoundError) as err:
             self.provider.get_value_for_secret(self.secret)
