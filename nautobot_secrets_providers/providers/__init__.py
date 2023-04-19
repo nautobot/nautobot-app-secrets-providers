48438e2dad69dbdf77391505ecf1067038c2df77
Bryan Culver <bryan.culver@networktocode.com>
2023-04-19 09:32:36 -0400

    Address PR feedback

--- nautobot_secrets_providers/providers/__init__.py
+++ nautobot_secrets_providers/providers/__init__.py
@@ -1,14 +1,13 @@
 """Nautobot Secrets Providers."""
 
-from .aws import AWSSecretsManagerSecretsProvider
-from .aws_ssm import AWSSystemsManagerParameterStore
+from .aws import AWSSecretsManagerSecretsProvider, AWSSystemsManagerParameterStore
 from .hashicorp import HashiCorpVaultSecretsProvider
 from .delinea import ThycoticSecretServerSecretsProviderId, ThycoticSecretServerSecretsProviderPath
 
-__all__ = (  # type: ignore
-    AWSSecretsManagerSecretsProvider,  # pylint: disable=invalid-all-object
-    HashiCorpVaultSecretsProvider,  # pylint: disable=invalid-all-object
-    AWSSystemsManagerParameterStore,  # pylint: disable=invalid-all-object
-    ThycoticSecretServerSecretsProviderId,  # pylint: disable=invalid-all-object
-    ThycoticSecretServerSecretsProviderPath,  # pylint: disable=invalid-all-object
+__all__ = (
+    "AWSSecretsManagerSecretsProvider",
+    "HashiCorpVaultSecretsProvider",
+    "AWSSystemsManagerParameterStore",
+    "ThycoticSecretServerSecretsProviderId",
+    "ThycoticSecretServerSecretsProviderPath",
 )
