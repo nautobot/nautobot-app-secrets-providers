6ef76f9ffb53f9cb78b884d32da0cca5c07851b0
bakebot <info@networktocode.com>
2023-08-01 11:53:36 +0000

    Cookie initialy baked
    Template:
    
    ```
    {
        "template": "https://github.com/networktocode-llc/cookiecutter-ntc.git",
        "dir": "nautobot-plugin",
        "ref": "nautobot-plugin-v1.0",
        "path": null
    }
    ```
    
    Cookie:
    
    ```
    {
        "remote": "https://github.com/nautobot/nautobot-plugin-secrets-providers.git",
        "path": "/opt/ntc/outputs/nautobot-plugin-secrets-providers",
        "repository_path": "/opt/ntc/outputs/nautobot-plugin-secrets-providers",
        "dir": "",
        "already_cloned": false,
        "branch_prefix": "drift-manager",
        "context": {
            "codeowner_github_usernames": "@smith-ntc",
            "full_name": "Network to Code, LLC",
            "email": "info@networktocode.com",
            "github_org": "nautobot",
            "base_name": "secrets_providers",
            "plugin_name": "nautobot_secrets_providers",
            "verbose_name": "Nautobot's Secrets Providers Plugin",
            "plugin_slug": "nautobot-secrets-providers",
            "project_slug": "nautobot-plugin-secrets-providers",
            "repo_url": "https://github.com/nautobot/nautobot-plugin-secrets-providers",
            "base_url": "secrets-providers",
            "min_nautobot_version": "1.4.0",
            "max_nautobot_version": "1.9999",
            "nautobot_version": "latest",
            "camel_name": "NautobotSecretsProviders",
            "project_short_description": "Nautobot's Secrets Providers Plugin",
            "version": "0.1.0",
            "model_class_name": "None",
            "open_source_license": "Apache-2.0",
            "docs_base_url": "https://docs.nautobot.com",
            "docs_app_url": "https://docs.nautobot.com/projects/secrets-providers/en/latest",
            "_template_directory": "nautobot-plugin",
            "_template": "/opt/ntc/networktocode-llc/cookiecutter-ntc",
            "_output_dir": "/opt/ntc/baked",
            "_drift_manager": {
                "template": "https://github.com/networktocode-llc/cookiecutter-ntc.git",
                "template_dir": "nautobot-plugin",
                "template_ref": "nautobot-plugin-v1.0",
                "cookie_dir": "",
                "branch_prefix": "drift-manager"
            }
        },
        "base_branch": "develop",
        "remote_name": "origin"
    }
    ```
    
    CLI Arguments:
    
    ```
    {
        "cookie_dir": "",
        "input": false,
        "json_filename": "/opt/ntc/nautobot-plugin-secrets-providers.cookiecutter.json",
        "output_dir": "/opt/ntc/outputs",
        "push": false,
        "template": "",
        "template_dir": "",
        "template_ref": ""
    }
    ```

---
+++ nautobot_secrets_providers/__init__.py
@@ -0,0 +1,30 @@
+"""Plugin declaration for nautobot_secrets_providers."""
+# Metadata is inherited from Nautobot. If not including Nautobot in the environment, this should be added
+try:
+    from importlib import metadata
+except ImportError:
+    # Python version < 3.8
+    import importlib_metadata as metadata
+
+__version__ = metadata.version(__name__)
+
+from nautobot.extras.plugins import PluginConfig
+
+
+class NautobotSecretsProvidersConfig(PluginConfig):
+    """Plugin configuration for the nautobot_secrets_providers plugin."""
+
+    name = "nautobot_secrets_providers"
+    verbose_name = "Nautobot's Secrets Providers Plugin"
+    version = __version__
+    author = "Network to Code, LLC"
+    description = "Nautobot's Secrets Providers Plugin."
+    base_url = "secrets-providers"
+    required_settings = []
+    min_version = "1.4.0"
+    max_version = "1.9999"
+    default_settings = {}
+    caching_config = {}
+
+
+config = NautobotSecretsProvidersConfig  # pylint:disable=invalid-name
---
+++ nautobot_secrets_providers/tests/__init__.py
@@ -0,0 +1,1 @@
+"""Unit tests for nautobot_secrets_providers plugin."""
