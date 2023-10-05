4288ef3007c8dcd77f8542fed5ed4be2280196d9
Jan Snasel <jan.snasel@networktocode.com>
2023-10-05 08:08:07 +0000

    Cookie updated by NetworkToCode Cookie Drift Manager Tool
    Template:
    
    ```
    {
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "dir": "nautobot-app",
        "ref": "u/snaselj-ssot",
        "path": null
    }
    ```
    
    Cookie:
    
    ```
    {
        "remote": "https://github.com/nautobot/nautobot-plugin-secrets-providers.git",
        "path": "/opt/ntc/drift-manager/outputs/nautobot-plugin-secrets-providers",
        "repository_path": "/opt/ntc/drift-manager/outputs/nautobot-plugin-secrets-providers",
        "dir": "",
        "branch_prefix": "drift-manager",
        "context": {
            "codeowner_github_usernames": "@smith-ntc",
            "full_name": "Network to Code, LLC",
            "email": "info@networktocode.com",
            "github_org": "nautobot",
            "plugin_name": "nautobot_secrets_providers",
            "verbose_name": "Nautobot's Secrets Providers Plugin",
            "plugin_slug": "nautobot-secrets-providers",
            "project_slug": "nautobot-plugin-secrets-providers",
            "repo_url": "https://github.com/nautobot/nautobot-plugin-secrets-providers",
            "base_url": "secrets-providers",
            "min_nautobot_version": "1.4.0",
            "max_nautobot_version": "1.9999",
            "nautobot_version": "latest",
            "camel_name": "NautobotSecretsProviders",
            "project_short_description": "Nautobot's Secrets Providers Plugin",
            "version": "0.1.0",
            "model_class_name": "None",
            "open_source_license": "Apache-2.0",
            "docs_base_url": "https://docs.nautobot.com",
            "docs_app_url": "https://docs.nautobot.com/projects/secrets-providers/en/latest",
            "_template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
            "_output_dir": "/opt/ntc/drift-manager/outputs",
            "_repo_dir": "/home/jsn/.cookiecutters/cookiecutter-nautobot-app/nautobot-app",
            "_checkout": "u/snaselj-ssot",
            "_drift_manager": {}
        },
        "base_branch": "develop",
        "remote_name": "origin",
        "pull_request_strategy": "PullRequestStrategy.CREATE",
        "post_actions": [
            "PostAction.BLACK"
        ],
        "baked_commit_ref": ""
    }
    ```
    
    CLI Arguments:
    
    ```
    {
        "cookie_dir": "",
        "input": false,
        "json_filename": "",
        "output_dir": "./outputs",
        "push": true,
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "template_dir": "nautobot-app",
        "template_ref": "u/snaselj-ssot",
        "pull_request": null,
        "post_action": [
            "black"
        ],
        "disable_post_actions": false,
        "draft": true
    }
    ```

--- development/nautobot_config.py
+++ development/nautobot_config.py
@@ -4,18 +4,22 @@ import sys
 
 from django.core.exceptions import ImproperlyConfigured
 from nautobot.core.settings import *  # noqa: F403  # pylint: disable=wildcard-import,unused-wildcard-import
-from nautobot.core.settings_funcs import parse_redis_connection, is_truthy
+from nautobot.core.settings_funcs import is_truthy, parse_redis_connection
 
-# Enforce required configuration parameters
-for key in [
-    "NAUTOBOT_ALLOWED_HOSTS",
-    "NAUTOBOT_SECRET_KEY",
-    "POSTGRES_DB",
-    "POSTGRES_PASSWORD",
-    "POSTGRES_USER",
-]:
-    if not os.environ.get(key):
-        raise ImproperlyConfigured(f"Required environment variable {key} is missing.")
+#
+# Debug
+#
+
+DEBUG = is_truthy(os.getenv("NAUTOBOT_DEBUG", False))
+_TESTING = len(sys.argv) > 1 and sys.argv[1] == "test"
+
+if DEBUG and not _TESTING:
+    DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": lambda _request: True}
+
+    if "debug_toolbar" not in INSTALLED_APPS:  # noqa: F405
+        INSTALLED_APPS.append("debug_toolbar")  # noqa: F405
+    if "debug_toolbar.middleware.DebugToolbarMiddleware" not in MIDDLEWARE:  # noqa: F405
+        MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")  # noqa: F405
 
 #
 # Misc. settings
@@ -24,6 +28,9 @@ for key in [
 ALLOWED_HOSTS = os.getenv("NAUTOBOT_ALLOWED_HOSTS", "").split(" ")
 SECRET_KEY = os.getenv("NAUTOBOT_SECRET_KEY", "")
 
+#
+# Database
+#
 
 nautobot_db_engine = os.getenv("NAUTOBOT_DB_ENGINE", "django.db.backends.postgresql")
 default_db_settings = {
@@ -53,17 +60,28 @@ if DATABASES["default"]["ENGINE"] == "django.db.backends.mysql":
     DATABASES["default"]["OPTIONS"] = {"charset": "utf8mb4"}
 
 #
-# Debug
+# Redis
 #
 
-DEBUG = is_truthy(os.getenv("NAUTOBOT_DEBUG", False))
-TESTING = len(sys.argv) > 1 and sys.argv[1] == "test"
+# The django-redis cache is used to establish concurrent locks using Redis.
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": parse_redis_connection(redis_database=0),
+        "TIMEOUT": 300,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
 
-if DEBUG and not TESTING:
-    if "debug_toolbar" not in INSTALLED_APPS:  # noqa: F405
-        INSTALLED_APPS.append("debug_toolbar")  # noqa: F405
-    if "debug_toolbar.middleware.DebugToolbarMiddleware" not in MIDDLEWARE:  # noqa: F405
-        MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")  # noqa: F405
+# Redis Cacheops
+CACHEOPS_REDIS = parse_redis_connection(redis_database=1)
+
+#
+# Celery settings are not defined here because they can be overloaded with
+# environment variables. By default they use `CACHES["default"]["LOCATION"]`.
+#
 
 #
 # Logging
@@ -72,7 +90,7 @@ if DEBUG and not TESTING:
 LOG_LEVEL = "DEBUG" if DEBUG else "INFO"
 
 # Verbose logging during normal development operation, but quiet logging during unit test execution
-if not TESTING:
+if not _TESTING:
     LOGGING = {
         "version": 1,
         "disable_existing_loggers": False,
@@ -108,68 +126,17 @@ if not TESTING:
     }
 
 #
-# Redis
+# Apps
 #
 
-# The django-redis cache is used to establish concurrent locks using Redis. The
-# django-rq settings will use the same instance/database by default.
-#
-# This "default" server is now used by RQ_QUEUES.
-# >> See: nautobot.core.settings.RQ_QUEUES
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": parse_redis_connection(redis_database=0),
-        "TIMEOUT": 300,
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        },
-    }
-}
-
-# RQ_QUEUES is not set here because it just uses the default that gets imported
-# up top via `from nautobot.core.settings import *`.
-
-# Redis Cacheops
-CACHEOPS_REDIS = parse_redis_connection(redis_database=1)
-
-#
-# Celery settings are not defined here because they can be overloaded with
-# environment variables. By default they use `CACHES["default"]["LOCATION"]`.
-#
-
-# Enable installed plugins. Add the name of each plugin to the list.
+# Enable installed Apps. Add the name of each App to the list.
 PLUGINS = ["nautobot_secrets_providers"]
 
-# Plugins configuration settings. These settings are used by various plugins that the user may have installed.
-# Each key in the dictionary is the name of an installed plugin and its value is a dictionary of settings.
-
-PLUGINS_CONFIG = {
-    "nautobot_secrets_providers": {
-        "hashicorp_vault": {
-            "url": os.environ.get("VAULT_URL"),
-            "token": os.environ.get("VAULT_TOKEN"),
-        },
-        "thycotic": {  # https://github.com/thycotic/python-tss-sdk
-            "base_url": os.getenv("SECRET_SERVER_BASE_URL"),
-            "cloud_based": is_truthy(os.getenv("SECRET_SERVER_IS_CLOUD_BASED", "False")),
-            # tenant: required when cloud_based == True
-            "tenant": os.getenv("SECRET_SERVER_TENANT", ""),
-            # Setup thycotic authorizer
-            # Username | Password | Token | Domain | Authorizer
-            #   def    |   def    |   *   |   -    | PasswordGrantAuthorizer
-            #   def    |   def    |   *   |  def   | DomainPasswordGrantAuthorizer
-            #    -     |    -     |  def  |   *    | AccessTokenAuthorizer
-            #   def    |    -     |  def  |   *    | AccessTokenAuthorizer
-            #    -     |   def    |  def  |   *    | AccessTokenAuthorizer
-            "username": os.getenv("SECRET_SERVER_USERNAME", ""),
-            "password": os.getenv("SECRET_SERVER_PASSWORD", ""),
-            "token": os.getenv("SECRET_SERVER_TOKEN", ""),
-            "domain": os.getenv("SECRET_SERVER_DOMAIN", ""),
-            # ca_bundle_path: (optional) Path to trusted certificates file.
-            #     This must be set as environment variable.
-            #     see: https://docs.python-requests.org/en/master/user/advanced/
-            "ca_bundle_path": os.getenv("REQUESTS_CA_BUNDLE", ""),
-        },
-    },
-}
+# Apps configuration settings. These settings are used by various Apps that the user may have installed.
+# Each key in the dictionary is the name of an installed App and its value is a dictionary of settings.
+# PLUGINS_CONFIG = {
+#     'nautobot_secrets_providers': {
+#         'foo': 'bar',
+#         'buzz': 'bazz'
+#     }
+# }
