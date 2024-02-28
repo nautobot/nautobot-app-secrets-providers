d02e60cfd13da7984d610cd9c7fa1fc61c5b7238
Hector GODZIEN <hgodzien@gmail.com>
2024-02-28 23:57:48 +0100

    Added CyberARK provider

--- nautobot_secrets_providers/providers/__init__.py
+++ nautobot_secrets_providers/providers/__init__.py
@@ -3,10 +3,12 @@
 from .aws import AWSSecretsManagerSecretsProvider, AWSSystemsManagerParameterStore
 from .hashicorp import HashiCorpVaultSecretsProvider
 from .delinea import ThycoticSecretServerSecretsProviderId, ThycoticSecretServerSecretsProviderPath
+from .cyberark import CyberARKSecretsProvider
 
 __all__ = (  # type: ignore
     AWSSecretsManagerSecretsProvider,  # pylint: disable=invalid-all-object
     AWSSystemsManagerParameterStore,  # pylint: disable=invalid-all-object
+    CyberARKSecretsProvider, # pylint: disable=invalid-all-object
     HashiCorpVaultSecretsProvider,  # pylint: disable=invalid-all-object
     ThycoticSecretServerSecretsProviderId,  # pylint: disable=invalid-all-object
     ThycoticSecretServerSecretsProviderPath,  # pylint: disable=invalid-all-object
