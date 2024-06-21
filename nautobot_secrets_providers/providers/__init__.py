1cdd6a293698448be3b86f59d8798b589c63c213
Steven Weiblinger <111259311+DistantVoyager@users.noreply.github.com>
2024-06-21 17:35:00 -0400

    Additional minor formatting

--- nautobot_secrets_providers/providers/__init__.py
+++ nautobot_secrets_providers/providers/__init__.py
@@ -1,15 +1,15 @@
 """Nautobot Secrets Providers."""
 
 from .aws import AWSSecretsManagerSecretsProvider, AWSSystemsManagerParameterStore
+from .azure import AzureKeyVaultSecretsProvider
 from .hashicorp import HashiCorpVaultSecretsProvider
 from .delinea import ThycoticSecretServerSecretsProviderId, ThycoticSecretServerSecretsProviderPath
-from .azure import AzureKeyVaultSecretsProvider
 
 __all__ = (  # type: ignore
     AWSSecretsManagerSecretsProvider,  # pylint: disable=invalid-all-object
     AWSSystemsManagerParameterStore,  # pylint: disable=invalid-all-object
+    AzureKeyVaultSecretsProvider,  # pylint: disable=invalid-all-object
     HashiCorpVaultSecretsProvider,  # pylint: disable=invalid-all-object
     ThycoticSecretServerSecretsProviderId,  # pylint: disable=invalid-all-object
     ThycoticSecretServerSecretsProviderPath,  # pylint: disable=invalid-all-object
-    AzureKeyVaultSecretsProvider,  # pylint: disable=invalid-all-object
 )
